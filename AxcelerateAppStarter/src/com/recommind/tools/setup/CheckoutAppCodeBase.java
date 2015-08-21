3dc453f82c02b82bb840c234ffbb01cc1e4e8b87
AKSahu <aswinikumar.89@gmail.com>
2015-08-21 07:42:46 +0200

    Added classes for testing prerequisites for the tool and checking out application code base

---
+++ AxcelerateAppStarter/src/com/recommind/tools/setup/CheckoutAppCodeBase.java
@@ -0,0 +1,5 @@
+package com.recommind.tools.setup;
+
+public class CheckoutAppCodeBase {
+
+}
---
+++ AxcelerateAppStarter/src/com/recommind/tools/setup/TestPrerequisites.java
@@ -0,0 +1,10 @@
+package com.recommind.tools.setup;
+
+/**
+ * This class checks if all the prerequisites for the tool(e.g. system environment details etc.)
+ * @author asu
+ *
+ */
+public class TestPrerequisites {
+
+}
