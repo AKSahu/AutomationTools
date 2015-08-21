852293068c21f123a5277ef05f45a38033ec2d80
AKSahu <aswinikumar.89@gmail.com>
2015-08-21 07:38:48 +0200

    Added configuration properties and working on starting and stopping launcher service

--- AxcelerateAppStarter/src/com/recommind/tools/services/LauncherService.java
+++ AxcelerateAppStarter/src/com/recommind/tools/services/LauncherService.java
@@ -1,5 +1,37 @@
 package com.recommind.tools.services;
 
+import java.io.File;
+
+import com.recommind.tools.utils.ConfigUtil;
+
+/**
+ * This class models the starting and stopping launcher service for the
+ * Axcelerate application
+ * 
+ * @author asu
+ *
+ */
 public class LauncherService {
 
+	/**
+	 * Starts the launcher service
+	 */
+	public void startLauncherService() {
+
+		String workspaceDir = ConfigUtil.getProperty("workspace_dir");
+		String mindserverDir = workspaceDir + File.separator
+				+ "mindserver-projects";
+		mindserverDir = mindserverDir.replace("\\", File.separator).replace(
+				"/", File.separator);
+
+		// TODO
+	}
+
+	/**
+	 * Stops the launcher service
+	 */
+	public void stopLauncherService() {
+		
+		// TODO
+	}
 }
