e1ad2ce6cc84a1f35d3f10ff0be60e9f660beb35
AKSahu <aswinikumar.89@gmail.com>
2015-08-23 13:49:23 +0200

    Added keyword for executing a windows command and returning the exit value

--- AxcelerateAppStarter/src/com/recommind/tools/utils/ProcessExecutor.java
+++ AxcelerateAppStarter/src/com/recommind/tools/utils/ProcessExecutor.java
@@ -1,12 +1,13 @@
 package com.recommind.tools.utils;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * This class models the execution of various commands or process and returns the output
- * This will be used by all the service classes
+ * This class models the execution of various commands or process and returns
+ * the output This will be used by all the service classes
  * 
  * @author asu, nkr
  *
@@ -58,16 +59,57 @@ public class ProcessExecutor {
 
 		return output.toString();
 	}
-	
+
+	/**
+	 * Executes any windows command and returns an exit value
+	 * 
+	 * @author asu
+	 * @param directory
+	 * @param aCommand
+	 * @return
+	 */
+	public int executeCommand(String directory, String aCommand) {
+		int exitValue = -1;
+		try {
+			ProcessBuilder probuilder = new ProcessBuilder("CMD", "/C",
+					aCommand);
+			probuilder.directory(new File(directory));
+			Process process;
+
+			process = probuilder.start();
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					process.getInputStream()));
+			String line;
+			System.out.printf("Output of running command is:\n");
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);// need to add in logger
+			}
+
+			exitValue = process.waitFor();
+		} catch (IOException e) {
+			System.err.println(e.getMessage());// need to add in logger
+		} catch (InterruptedException e) {
+			System.err.println(e.getMessage());// need to add in logger
+		}
+		return exitValue;
+	}
+
 	/**
 	 * Usage testing
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		ProcessExecutor pe = new ProcessExecutor();
 		String aCommand = "ping aksahu.blogspot.in";
 		String output = pe.executeCommand(aCommand);
-		System.out.println("The Output is:"+output);
+		System.out.println("The Output is:" + output);
+
+		int exitValue = new ProcessExecutor().executeCommand(
+				"G://aksahu_ws/UtilityAKSahu", "dir");
+		// int exitValue = new ProcessExecutor().executeCommand("D://", "dir");
+		System.out.println("Exit value is:" + exitValue);
 	}
 
 }
